3f5ddc18eee418dc2a91c51f22f232d03ea2ef1d
karlssoj <karlssoj@arcada.fi>
2024-11-13 10:55:41 +0200

    Added collision functions

--- Assets/Scripts/Force.cs
+++ Assets/Scripts/Force.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Force : MonoBehaviour
@@ -9,6 +10,10 @@ public class Force : MonoBehaviour
     Vector2 Velocity;
 
     Rigidbody2D PhysicsEngine;
+
+    bool OnGround = false;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,14 +28,43 @@ public class Force : MonoBehaviour
         //Utsätter bilen för en kontinuerlig "push-kraft" rakt till höger
         PhysicsEngine.AddForce(PushForce);
 
-
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if(Input.GetKeyDown(KeyCode.Space) == true && OnGround == true)
         {
             //Utsätter bilen för en impulskraft rakt uppåt. 
             //(kraften multipliceras inte med time.deltatime)
             PhysicsEngine.AddForce(JumpForce, ForceMode2D.Impulse);
         }
+    }
 
+    //Anropas av spelmotorn när i detta fall bilens collider kör in i ett
+    //annat spelobjekts collid
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        //if-satsen blir true om spelobjektet som hänger ihop med den collider
+        //vi krockat med heter "landscape"
+        if(c.gameObject.name == "landscape")
+        {
+                OnGround = true;
+        }
+    }
+
+
+    //Anropas av spelmotorn när i detta fall bilens collider lämnar ett annat
+    //spelobjekt collider
+    private void OnCollisionExit2D(Collision2D c)
+    {
+        if(c.gameObject.name == "landscape")
+        {
+                OnGround = false;
+        }     
+    }
+
+    //Anropas av spelmotorn när i detta fall bilens collider kör in i ett
+    //annat spelobjekts collider som är definierad som en trigger (isTrigger 
+    //förkruxat under BoxCollider2D-kompoenenten)
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        Debug.Log("I MÅL!!!!!");
     }
 }
 
