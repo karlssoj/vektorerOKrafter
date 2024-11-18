87012da111f392d70eb53a5bddb9ec6639e73d44
karlssoj <karlssoj@arcada.fi>
2024-11-18 10:44:25 +0200

    Added gravitaional force script

---
+++ Assets/Scripts/GravitationalForce.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class GravitationalForce : MonoBehaviour
+{
+    //Den universella proportionalitetskonstanten. 
+    //Definieras via Unity-editorn i inspektorn. Här behöver vi inte sätta den räta konstatenten
+    //eftersom då går allt väldigt långsamt (låg acceleration). Ju lägre värde desto
+    //fortare accerleras månen
+    public float G; 
+
+    Rigidbody2D EarthPhysics;
+    Rigidbody2D MoonPhysics;
+    GameObject Moon;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Hämtar en referens till jordens "fysikmotor"
+        EarthPhysics = GetComponent<Rigidbody2D>();
+        
+        //Hämtar en referens till månens spelobjekt
+        Moon = GameObject.Find("Moon");
+        
+        //Hämtar en referens till månens fysikmotor via månens spelobjekt
+        MoonPhysics = Moon.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Beräknr avståndet mellan månen och jorden för varje frame
+        float r = Vector2.Distance(transform.position, Moon.transform.position);
+
+        //Beräknar gravitationskraften enligt Newtons gravitationslag. Jordens och månens
+        //massor är definierade i månes och jordens Rigidbody2D-komponenter
+        float F = G*((MoonPhysics.mass * EarthPhysics.mass)/(r*r));
+
+        //Skapr en vektor Direction för att beskriva riktningen från månen till jorden
+        //så att vi vet åt vilket håll vi ska rikta gravitationskraften
+        Vector2 Direction =  transform.position - Moon.transform.position;
+        
+        //Normaliserar vektorn (för att få längden till 1) och multiplicerar med gravitationskraften. 
+        //Då får vi en kraft F som är riktad från månen till jorden. 
+        Direction.Normalize();
+        Vector2 Force = Direction * F;
+
+        //Utsätter månen för gravitationskraften
+        MoonPhysics.AddForce(Force);
+    }
+}
